ecf2e0436986c5b7e01f94f48d82b8459ca29d2c
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-10-20 15:27:28 +0300

    Make RAM allocation accept hex arguments!!

--- source/lib/macros.asm
+++ source/lib/macros.asm
@@ -15,11 +15,14 @@ macro allocateRAM(page, ...)
     if <page> >= $200 || <page> < 0
         error "RAM page <page> is outside the allowed bounds of $000..$1FF."
     endif
-	if not(defined("RAM_LOCATION_<page>"))
-		!RAM_LOCATION_<page> #= $7E0000+(<page>*$100)
-	endif
-	org !RAM_LOCATION_<page>
-	
+
+    !__allocateRAM_page #= <page>
+    !__allocateRAM_RAM_LOCATION = "RAM_LOCATION_!__allocateRAM_page"
+	if not(defined("!__allocateRAM_RAM_LOCATION"))
+		!{!__allocateRAM_RAM_LOCATION} #= $7E0000+(<page>*$100)
+    endif
+	org !{!__allocateRAM_RAM_LOCATION}
+
     ; Actually put the data
     !__allocateRAM_cnt #= 0
     while !__allocateRAM_cnt < sizeof(...)
@@ -27,12 +30,14 @@ macro allocateRAM(page, ...)
 		!__allocateRAM_cnt #= !__allocateRAM_cnt+2
 	endwhile
 
-	!RAM_LOCATION_<page> #= pc()
-	if !RAM_LOCATION_<page> >= $7E0000+((<page>+1)*$100)
+	!{!__allocateRAM_RAM_LOCATION} #= pc()
+	if !{!__allocateRAM_RAM_LOCATION} > $7E0000+((<page>+1)*$100)
 		warn "Page <page> of RAM is full"
+    elseif !{!__allocateRAM_RAM_LOCATION} == $7E0000+((<page>+1)*$100)
+        print "Page <page> of RAM has been filled"
 	endif
-    if !RAM_LOCATION_<page> >= $7E0000+(((<page>&$100)+$100)*$100)
-        error "Page <page> of RAM has crossed the bank boundary into" $hex(!RAM_LOCATION_<page>, 6)
+    if !{!__allocateRAM_RAM_LOCATION} >= $7E0000+(((<page>&$100)+$100)*$100)
+        error "Page <page> of RAM has crossed the bank boundary into" $hex(!{!__allocateRAM_RAM_LOCATION}, 6)
     endif
 	pullpc
 endmacro
