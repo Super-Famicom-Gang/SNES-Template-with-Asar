6c257cab3c0ddf84adb53d60839858e24e566d46
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-10-27 13:50:37 +0300

    Redo and fix RAM allocations 8 16-bit registers, with hi_lo bytes and d and q aliases

--- source/lib/baseLibMem.asm
+++ source/lib/baseLibMem.asm
@@ -1,17 +1,15 @@
 ; Allocate pseudo registers
-for i = 0..16
-%allocateRAM(0, br!i, 1)
-endfor
 for i = 0..8
-!t #= !i*2
-wr!i = r!t
+%allocateRAM(0, r!i, 2)
+r!{i}_lo = r!i
+r!{i}_hi = r!{i}+1
 endfor
 for i = 0..4
-!t #= !i*4
+!t #= !i*2
 dr!i = r!t
 endfor
-qr0 = br0
-qr1 = br8
+qr0 = r0
+qr1 = r4
 
 ; Allocate dma buffer
 %allocateRAM($1C0, boundCheck, 0, ==, dmaBuffer, $200)
